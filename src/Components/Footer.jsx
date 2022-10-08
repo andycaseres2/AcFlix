829ad6ea16999122472fe090e82abf74492d833b
Andy Caseres <andycaseres2@gmail.com>
2022-10-07 22:57:19 -0500

    correction footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -17,15 +17,15 @@ const scrollToTop = () => {
 
 export const Footer = () => {
   return (
-    <div className="h-44 w-full mt-6 bg-black relative ">
-      <div className={"flex gap-4 p-4"}>
+    <div className="h-44 w-full mt-6 bg-black relative">
+      <div className={"flex gap-4 p-4 w-full justify-center "}>
         <AiOutlineInstagram className="h-10 w-10 fill-white hover:scale-110 ease-in-out duration-300 cursor-pointer" />
         <AiOutlineTwitter className="h-10 w-10 fill-white hover:scale-110 cursor-pointer ease-in-out duration-300" />
         <AiFillLinkedin className="h-10 w-10 fill-white hover:scale-110 cursor-pointer ease-in-out duration-300" />
       </div>
       <AiOutlineArrowUp
         onClick={() => scrollToTop()}
-        className="h-10 w-10 absolute top-4 right-4 fill-white animate-bounce cursor-pointer"
+        className="h-10 w-10 absolute top-4 right-4 md:right-10 fill-white animate-bounce cursor-pointer"
       />
     </div>
   );
